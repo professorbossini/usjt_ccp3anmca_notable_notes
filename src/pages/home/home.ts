dcdc21d1b196084533f2f56f07748d836cc49cd7
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-05-08 22:21:13 -0300

    terminamos a primeira parte do pdf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NoteService } from '../../app/note.service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notes;
 
+  constructor(public navCtrl: NavController, private noteService: NoteService) {
+    
+  }
+  onItemClick (note){
+    console.log ('item-click', note);
+    this.navCtrl.push('DetailPage', {
+      noteParam: note
+    });
   }
 
 }
